d0009cabb41a20b6aa58802de6bae39cd40c2f5b
Tianyi Zhang <zhang.tianyi9@northeastern.edu>
2023-12-01 00:44:48 -0800

    Revise job logic

--- check_and_submit.sh
+++ check_and_submit.sh
@@ -11,29 +11,39 @@ update_job_name_and_checkpoint() {
     local latest_epoch=$(ls epoch-*.pt 2>/dev/null | sort -V | tail -n 1)
     local latest_batch=$(ls checkpoint-*.pt 2>/dev/null | sort -V | tail -n 1)
 
-    if [[ -n $latest_epoch ]] && [[ -z $latest_batch || $latest_epoch -nt $latest_batch ]]; then
+    # Default values
+    local start_epoch_arg="--start-epoch 1"
+    local start_batch_arg="--start-batch 0"
+    local job_name="train_1_0"
+    local epoch_num="1"
+    if [[ -n $latest_epoch ]]; then
         epoch_num=$(echo $latest_epoch | grep -o -E '[0-9]+')
-        checkpoint_args="--start-epoch $((epoch_num + 1))"
-        job_name="train_${epoch_num}_end"
+        start_epoch_arg="--start-epoch $((epoch_num + 1))"
 
-        # Check if max epochs have been reached
         if (( epoch_num >= max_epochs )); then
             echo "Maximum number of epochs ($max_epochs) reached at $(date). No further action required."
             exit 0
         fi
-    elif [[ -n $latest_batch ]]; then
-        batch_num=$(echo $latest_batch | grep -o -E '[0-9]+')
-        checkpoint_args="--start-batch $batch_num"
-        job_name="train_0_${batch_num}"
-    else
-        checkpoint_args="--start-epoch 1 --start-batch 0"
-        job_name="train_0_0"
     fi
 
-    # Update job name and checkpoint in the train_job.sh script
+    if [[ -n $latest_batch ]]; then
+        batch_num=$(echo $latest_batch | grep -o -E '[0-9]+')
+        start_batch_arg="--start-batch $batch_num"
+        
+        if [[ -n $latest_epoch ]]; then
+            if [[ $latest_epoch -nt $latest_batch ]]; then
+                start_batch_arg=""
+            fi
+            job_name="train_${epoch_num}_${batch_num}"
+        fi
+    fi
+
+    # Update job name, start epoch, and start batch in the train_job.sh script
     sed -i "s/#SBATCH --job-name=.*/#SBATCH --job-name=$job_name/" $valle_root/../train_job.sh
-    sed -i "s/--start-epoch [0-9]* --start-batch [0-9]*/$checkpoint_args/" $valle_root/../train_job.sh
+    sed -i "s/--start-epoch [0-9]*/$start_epoch_arg/" $valle_root/../train_job.sh
+    sed -i "s/--start-batch [0-9]*/$start_batch_arg/" $valle_root/../train_job.sh
 }
+
 
 # Check if there are running or pending jobs
 if squeue -u `whoami` | grep -E " R| PD" > /dev/null; then
